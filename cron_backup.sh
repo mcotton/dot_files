82ce730b60afe58d2e717d29405e4c7ef3b3abcb
mcotton <tifosigoon@gmail.com>
2020-04-14 23:32:37 -0500

    latest changes

--- cron_backup.sh
+++ cron_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -a --exclude Storage --exclude Storage/* --exclude .cache --exclude .config  --exclude .dbus --progress --stats --human-readable /home/mcotton /mnt/Storage/backup/
+rsync -a --exclude Storage --exclude Storage/* --exclude .cache --exclude .config  --exclude .dbus --progress --stats --human-readable /home/mcotton /mnt/md0/backup/
